c9977504420ce95c6537deb30ea26234b4bb2592
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-04-19 13:37:17 +1200

    dynamic images and film type with javascript object

--- angular.js
+++ angular.js
@@ -23,31 +23,66 @@ dylanApp.directive('imageonload', function() {
     })
 
 dylanApp.controller('mainController', function($scope, $sce) {
-    // $scope.gallery = {
-    //     1: {
-    //         file: 'one.jpg',
-    //         film_type: ""
-    //     }
-    // }
-    $scope.gallery = []
+    $scope.gallery = {
+        1: {
+            file: 'one.jpg',
+            film_type: "Portra 160"
+        },
+        2: {
+            file: 'two.jpg',
+            film_type: "Portra 160"
+        },
+        3: {
+            file: 'three.jpg',
+            film_type: "Portra 160"
+        },
+        4: {
+            file: 'four.jpg',
+            film_type: "Superia 800"
+        },
+        5: {
+            file: 'five.jpg',
+            film_type: "Superia 800"
+        },
+        6: {
+            file: 'six.jpg',
+            film_type: "Superia 800"
+        },
+        7: {
+            file: 'seven.jpg',
+            film_type: "Superia 800"
+        },
+        8: {
+            file: 'eight.jpg',
+            film_type: "Superia 800"
+        },
+        9: {
+            file: 'nine.jpg',
+            film_type: "Superia 800"
+        },
+        10: {
+            file: 'ten.jpg',
+            film_type: "Superia 400"
+        },
+        11: {
+            file: 'eleven.jpg',
+            film_type: "Superia 400"
+        },
+        12: {
+            file: 'twelve.jpg',
+            film_type: "Portra 400"
+        },
+        13: {
+            file: 'thirteen.jpg',
+            film_type: "Superia 800"
+        }      
+    }
+    console.log('test')
+    console.log($scope.gallery[1])
+
     $scope.index = 0
     $scope.colorThief = new ColorThief
     $scope.colors = []
-    // $scope.imageUrl = '../imgs/' + $scope.gallery[$scope.index]
-
-    $scope.trusted = function(url) {
-        return $sce.trustAsResourceUrl(url);
-    }
-
-    $scope.populateArray = function(num) {
-        n = 0
-        for (var i = num - 1; i >= 0; i--) {
-            n++
-            $scope.gallery.push(n + '.jpg')
-        }
-    }
-
-    $scope.populateArray(14)
 
     $scope.go = function (path) {
       $location.path(path);
@@ -62,10 +97,7 @@ dylanApp.controller('mainController', function($scope, $sce) {
     }
 
     $scope.colorBar = function () {
-        // console.log('-webkit-linear-gradient(left, ' + $scope.colors[0] + ' 0%, ' + $scope.colors[1] + ' 25%, ' + $scope.colors[2] + ' 50%, ' + $scope.colors[3] + ' 75%, ' + 'rgb(0,0,0)' + ' 100%)')
-        // $($('.color-bar')[0]).css({background: '-webkit-linear-gradient(top, ' + $scope.colors[0] + ' 0%, ' + $scope.colors[1] + ' 25%, ' + $scope.colors[2] + ' 50%, ' + $scope.colors[3] + ' 75%, ' + $scope.colors[4] + ' 100%)'})
         for (var i = 0; i < $('.color-bar').length; i++) {
-            console.log($scope.colors[0])
             $($('.color-bar')[i]).css({background: '-moz-linear-gradient(top, ' + $scope.colors[0] + ' 0%, ' + $scope.colors[1] + ' 25%, ' + $scope.colors[2] + ' 50%, ' + $scope.colors[3] + ' 75%, ' + $scope.colors[4] + ' 100%)'})
             $($('.color-bar')[i]).css({background: '-webkit-linear-gradient(top, ' + $scope.colors[0] + ' 0%, ' + $scope.colors[1] + ' 25%, ' + $scope.colors[2] + ' 50%, ' + $scope.colors[3] + ' 75%, ' + $scope.colors[4] + ' 100%)'})
             $($('.color-bar')[i]).css({background: 'linear-gradient(to bottom, ' + $scope.colors[0] + ' 0%, ' + $scope.colors[1] + ' 25%, ' + $scope.colors[2] + ' 50%, ' + $scope.colors[3] + ' 75%, ' + $scope.colors[4] + ' 100%)'})
